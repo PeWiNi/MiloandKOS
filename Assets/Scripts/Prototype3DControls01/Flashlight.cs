e18d75426daece04265b2bf0a981a149bfc454ef
henr9994 <henrikpedersen1@hotmail.com>
2014-10-14 11:56:18 +0200

    Milo Flashlight Control
    Implemented a flashlight control for Milo.
    Created a new scene called Prototype3DControls01
    where one can test it out, pressing 'F' will turn on and off the
    flashlight

---
+++ Assets/Scripts/Prototype3DControls01/Flashlight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour
+{
+    bool on = false;
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            on = !on;
+        }
+        if (on)
+        {
+            light.enabled = true;
+        } else
+        {
+            light.enabled = false;
+        }
+    }
+}
