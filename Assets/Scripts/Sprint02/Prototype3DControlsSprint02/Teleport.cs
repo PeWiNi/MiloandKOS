ff25bd10d4a3e6d7b54a4a2fe353add866e1ad9f
henr9994 <henrikpedersen1@hotmail.com>
2014-10-24 01:40:11 +0200

    Partly done KOS Teleport
    So the functionality is partly implemented, but it still need some work.
    I would like to have the ForeArm being the one stretching and moving
    attached with a secondary Camera to follow its movement.
    As the Teleport functions right now its more like flying than it is
    stretching xD.

---
+++ Assets/Scripts/Sprint02/Prototype3DControlsSprint02/Teleport.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour
+{
+    float mouseRotationX;
+    float mouseRotationY;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(1))//Right Mouse Button to control Rotation.
+        {
+            RotateForeArm();
+            if (Input.GetMouseButton(0))//Left Mouse Button to control Direction.
+            {
+                DirectionForeArm();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The rotation of the fore arm.
+    /// </summary>
+    void RotateForeArm()
+    {
+        mouseRotationX = Input.GetAxis("Mouse X");
+        mouseRotationY = Input.GetAxis("Mouse Y");
+        rigidbody.transform.RotateAround(Camera.main.transform.up, Mathf.Deg2Rad * mouseRotationX);
+        rigidbody.transform.RotateAround(Camera.main.transform.right, -Mathf.Deg2Rad * mouseRotationY);
+        Vector3 eulerAngles = transform.eulerAngles;
+        eulerAngles.z = 0.0f;
+        transform.eulerAngles = eulerAngles;
+    }
+
+    /// <summary>
+    /// The direction of the fore arm.
+    /// </summary>
+    void DirectionForeArm()
+    {
+        rigidbody.transform.localPosition += transform.forward * Time.deltaTime;//Works on Total body of KOS.
+    }
+}
