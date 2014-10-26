f3f29237f49e1c2128c7f3766a6219736527312c
henr9994 <henrikpedersen1@hotmail.com>
2014-10-26 17:54:00 +0100

    Teleport Done
    I think the teleport will work properly once the Idle animation comes
    into play.Iit is essential that the Idle state should avoid moving the
    arms in this case.
    Cheers! :D

--- Assets/Scripts/Sprint02/Prototype3DControlsSprint02/Teleport.cs
+++ Assets/Scripts/Sprint02/Prototype3DControlsSprint02/Teleport.cs
@@ -11,8 +11,12 @@ public class Teleport : MonoBehaviour
     Camera mainCamera;
     GameObject objectToFollow;
     GameObject foreArm;
+    GameObject KOS;
     Vector3 originalForeArmPosition;
     Quaternion originalForeArmRotation;
+    bool teleported = false;
+    bool isOriginalPositionAndRotationSet = false;
+    Animator anim;
 
     // Use this for initialization
     void Start()
@@ -21,38 +25,60 @@ public class Teleport : MonoBehaviour
         mainCamera = GameObject.Find("Main Camera").camera;
         objectToFollow = GameObject.Find("bind_R_Wrist01");
         foreArm = GameObject.Find("bind_R_ForeArm01");
-        originalForeArmPosition = foreArm.transform.position;
-        originalForeArmRotation = foreArm.transform.rotation;
+        KOS = GameObject.Find("KOSSprint02");//Should be 'KOS' in FINAL version.
+        anim = KOS.GetComponent<Animator>();
     }
 	
     // Update is called once per frame
     void Update()
     {
-        forearmCamera.transform.LookAt(objectToFollow.transform.position);
-        forearmCamera.transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y + 1.0f, objectToFollow.transform.position.z - 2.0f);
+        /* 
+         * As long as we don't want to teleport, set these values for the forearmCamera, 
+         * The values are the same as the Main Camera.
+         */
+        if (!Input.GetMouseButton(1))
+        {
+            forearmCamera.transform.LookAt(objectToFollow.transform.position);
+            forearmCamera.transform.rotation = mainCamera.transform.rotation;
+            forearmCamera.transform.position = mainCamera.transform.position;
+        }
     }
 
     void FixedUpdate()
     {
-        if (Input.GetMouseButton(1))//Right Mouse Button to control Rotation.
+        //If we are not currently moving, then proceed.
+        if (anim.GetFloat("Movement") == 0.0f)
         {
-            if (originalForeArmPosition == null)
+            if (!isOriginalPositionAndRotationSet)
             {
-                originalForeArmPosition = foreArm.transform.position;
+                SetOriginalPositionAndRotationValues();
             }
-            RotateForeArm();
-            mainCamera.enabled = false;
-            forearmCamera.enabled = true;
-            if (Input.GetMouseButton(0))//Left Mouse Button to control Direction.
+            //Right Mouse Button to control Rotation.
+            if (Input.GetMouseButton(1))
             {
-                DirectionForeArm();
+                RotateForeArm();
+
+                mainCamera.enabled = false;
+                forearmCamera.enabled = true;
+                //Left Mouse Button to control Direction.
+                if (Input.GetMouseButton(0))
+                {
+                    DirectionForeArm();
+                }
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    teleported = true;
+                }
+            } else if (!Input.GetMouseButton(1) && !teleported)
+            {
+                SetBackToOriginalPosAndRotValues();
+                mainCamera.enabled = true;
+                forearmCamera.enabled = false;
+            } 
+            if (!Input.GetMouseButton(1) && teleported)
+            {
+                TeleportToLocation();
             }
-        } else
-        {
-            mainCamera.enabled = true;
-            forearmCamera.enabled = false;
-            foreArm.transform.position = originalForeArmPosition;
-//            originalForeArmPosition = Vector3.;
         }
     }
 
@@ -61,10 +87,9 @@ public class Teleport : MonoBehaviour
     /// </summary>
     void RotateForeArm()
     {
+        forearmCamera.transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y + 1.0f, objectToFollow.transform.position.z - 2.0f);
         mouseRotationX = Input.GetAxis("Mouse X") * mouseSensitivityX;
         mouseRotationY = Input.GetAxis("Mouse Y") * mouseSensitivityY;
-//        rigidbody.transform.Rotate(Camera.main.transform.up, Mathf.Deg2Rad * mouseRotationX, Space.Self);
-//        rigidbody.transform.Rotate(Camera.main.transform.right, -Mathf.Deg2Rad * mouseRotationY, Space.Self);
         transform.Rotate(forearmCamera.transform.up, Mathf.Deg2Rad * mouseRotationX, Space.Self);
         transform.Rotate(forearmCamera.transform.right, -Mathf.Deg2Rad * mouseRotationY, Space.Self);
         Vector3 eulerAngles = transform.eulerAngles;
@@ -79,5 +104,37 @@ public class Teleport : MonoBehaviour
     {
 //        rigidbody.transform.localPosition += transform.forward * 5.0f * Time.deltaTime;//Works on Total body of KOS.
         rigidbody.MovePosition(rigidbody.position += new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 1.0f) * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Teleports to location.
+    /// </summary>
+    void TeleportToLocation()
+    {
+        Vector3 teleportationLocation = objectToFollow.transform.position;
+        SetOriginalPositionAndRotationValues();
+        SetBackToOriginalPosAndRotValues();
+        KOS.rigidbody.MovePosition(KOS.rigidbody.position += new Vector3(teleportationLocation.x, 0.0f, teleportationLocation.z));
+        teleported = false;
+    }
+
+    /// <summary>
+    /// Sets the original position and rotation values.
+    /// </summary>
+    void SetOriginalPositionAndRotationValues()
+    {
+        originalForeArmPosition = foreArm.transform.position;
+        originalForeArmRotation = foreArm.transform.rotation;
+        isOriginalPositionAndRotationSet = true;
+    }
+
+    /// <summary>
+    /// Sets the position and rotation back to the original values.
+    /// </summary>
+    void SetBackToOriginalPosAndRotValues()
+    {
+        foreArm.transform.position = originalForeArmPosition;
+        foreArm.transform.rotation = originalForeArmRotation;
+        isOriginalPositionAndRotationSet = false;
     }
 }
