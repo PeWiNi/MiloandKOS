f7dd0a4394df96d14fb4dd5cfb4e80acca76d755
henr9994 <henrikpedersen1@hotmail.com>
2014-10-27 08:45:59 +0100

    Camera Pan - !OBS NOT COMPLETE
    This feature is not yet complete and will need more work xD
    Cheers!

---
+++ Assets/Scripts/Sprint02/PrototypePanCameraToWalkSprint02/CameraPan.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour
+{
+    Vector3 originalCameraPosition;
+    Quaternion originalCameraRotation;
+    bool isMovingDown;
+    GameObject attachedTo;
+
+    // Use this for initialization
+    void Start()
+    {
+        attachedTo = GameObject.Find("MiloSprint02").gameObject;
+        originalCameraPosition = attachedTo.transform.position - transform.position;
+        originalCameraRotation = transform.rotation;
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S) && !isMovingDown)
+        {
+            isMovingDown = true;
+            attachedTo.transform.Rotate(0.0f, attachedTo.transform.rotation.y + 180.0f, 0.0f);
+            transform.position = attachedTo.transform.position - originalCameraPosition;
+            transform.rotation = originalCameraRotation;
+        } else if (!Input.GetKey(KeyCode.S) && isMovingDown)
+        {
+            isMovingDown = false;
+        }
+    }
+}
