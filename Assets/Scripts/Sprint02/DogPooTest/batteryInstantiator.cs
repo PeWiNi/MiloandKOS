ed7ad23dcfa996026dd24fabbf7cbb8d1fc61636
cristinaNiculescu <crissa_alessa@yahoo.com>
2014-10-28 11:55:32 +0100

    started Battery placement script
    will continue. also, i will do a lotus placement script

---
+++ Assets/Scripts/Sprint02/DogPooTest/batteryInstantiator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class battery : MonoBehaviour {
+	
+	public Transform batteryPrefab;
+	public string [,] batteryMap;
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	void drawBattery()
+	{
+		 
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
