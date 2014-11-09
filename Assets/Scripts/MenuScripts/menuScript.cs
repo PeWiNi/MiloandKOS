abeba4a59f56ce38dd2946df2cf07dc93b9b8f18
cristinaNiculescu <crissa_alessa@yahoo.com>
2014-11-09 18:30:45 +0100

    more on the menu
    needs more stuff :D

---
+++ Assets/Scripts/MenuScripts/menuScript.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class menuScript : MonoBehaviour {
+	//public GameObject MainCanvas;
+	//public GameObject Introim;
+	//public GameObject Menu;
+	bool menuShowed=false;
+	bool chosenMilo;
+
+
+	// Use this for initialization
+	void Start () {	
+		chosenMilo = true;
+		StartCoroutine (delayMenu ());
+	}
+
+	IEnumerator delayMenu()
+	{
+		while (!menuShowed) 
+		{
+			yield return new WaitForSeconds (5);
+			//wait a few second , then show the menu
+			showMenu ();
+		}
+	}
+	
+	public void showMenu()
+	{
+		menuShowed = true;
+		// Remove the intro pic
+		// Show the menu
+	}
+
+	public void pickedMilo ()
+	{ chosenMilo = true;
+	}
+	public void pickedKOS()
+	{ chosenMilo = false;
+	}
+
+	public bool ChosenCharacter
+	{
+		get
+		{
+			return chosenMilo;
+		}
+	}
+
+	public void newGame(){
+		//Application.LoadLevel ("IntroCutscene");
+		Application.LoadLevel ("DogPooTest");
+	}
+
+	public void exitGame(){
+		Application.Quit ();
+	}
+
+	public void optionsMenu()
+	{ //will follow 
+	}
+	public void loadGame()
+	{ //will follow
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
