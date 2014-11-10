bf1c3228cee8b3fd9a9d43890ec5cc0d5110837d
cristinaNiculescu <crissa_alessa@yahoo.com>
2014-11-10 15:22:13 +0100

    added particles from scene to lotus
    +the spritesSheets for the battle
    +tryouts on the cutscene

---
+++ Assets/Scripts/MenuScripts/loadLoadScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLoadScreen : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public static void loadLevel(string sceneName)
+	{
+		LoadingScreen46.show();
+		Application.LoadLevel(sceneName);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MenuScripts/loadingImages.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadingImages : MonoBehaviour {
+
+	GameObject [] imageList;
+	public string nameOfCutscene;
+	public int numberOFFrameInCutscene; 
+	int currentIndex;
+	public	string returnString;
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0;i<numberOFFrameInCutscene;i++)
+		imageList[i] = GameObject.Find (nameOfCutscene + toStringg(i));
+
+	}
+
+	public string toStringg(int num)
+	{	 
+		int zeros = 0;
+		while (num/10>=9) 
+		{
+			zeros++;
+			num/=10;
+		}
+		for (int i=0; i<zeros; i++) 
+		{	int n=0;
+			returnString+=n.ToString();
+		}
+		returnString += num.ToString ();
+		return returnString;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
