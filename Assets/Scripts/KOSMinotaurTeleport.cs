e8ce9b25ad15ffb9a884a396fb30af637e7fa0c2
henr9994 <henrikpedersen1@hotmail.com>
2014-11-11 18:50:52 +0100

    KOSMinotaur Teleport in progress
    Basic setup of the teleport mechanic, press 'T' to activate near trash
    cans

---
+++ Assets/Scripts/KOSMinotaurTeleport.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KOSMinotaurTeleport : MonoBehaviour
+{
+    GameObject kos;
+
+    // Use this for initialization
+    void Start()
+    {
+        kos = GameObject.Find("KOSMinotaur");
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
