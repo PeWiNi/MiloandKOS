c2c131d90a387c66f7b3c9a6ee46f8e7585c9ff9
GGBuzz <marcwgj@hotmail.com>
2014-11-12 20:28:53 +0100

    Startup on the final battle scenes

---
+++ Assets/Scripts/Final Battle/KOSThrowAxe.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KOSThrowAxe : MonoBehaviour
+{
+    Animator anim;
+    int axeThrow;
+    int notThrowing;
+    GameObject axe;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        axeThrow = Animator.StringToHash("AxeThrow");
+        notThrowing = Animator.StringToHash("NotThrowing");
+    }
+    // Use this for initialization
+    void Start()
+    {
+        axe = GameObject.Find("RotatingAxe01");
+        axe.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C)) //&& anim.GetBool(notThrowing))
+        {
+            anim.SetTrigger(axeThrow);
+            anim.SetBool(notThrowing, true);
+            axe.SetActive(true);
+            axe.rigidbody2D.AddForce(Vector2.up * 40 + Vector2.right * 100, ForceMode2D.Force);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "CannonBall01")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Final Battle/MiloShootCannonBall.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiloShootCannonBall : MonoBehaviour
+{
+    Animator anim;
+    int shootCannonBall;
+    GameObject cannonBall;
+    
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        shootCannonBall = Animator.StringToHash("ShootCannonBall");
+    }
+    // Use this for initialization
+    void Start()
+    {
+        cannonBall = GameObject.Find("CannonBall01");
+        cannonBall.SetActive(false);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C)) //&& anim.GetBool(notThrowing))
+        {
+            anim.SetTrigger(shootCannonBall);
+            cannonBall.SetActive(true);
+            cannonBall.rigidbody2D.AddForce(Vector2.up * 35 + Vector2.right * -100, ForceMode2D.Force);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "RotatingAxe01")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
