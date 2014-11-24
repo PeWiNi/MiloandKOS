28a2da5d6f9ca8c53f7d7e4baf454a292485401c
cristinaNiculescu <crissa_alessa@yahoo.com>
2014-11-24 14:01:54 +0100

    triggerpoints now show the proper images
    added second cameraPan script without "detectcollision" call (it's only
    commented out)

--- Assets/Scripts/triggerPointTutorial.cs
+++ Assets/Scripts/triggerPointTutorial.cs
@@ -1,23 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class triggerPointTutorial : MonoBehaviour {
-	public Texture infoPic;
-	public string message;
+public class triggerPointTutorial : MonoBehaviour
+{
+		public Texture infoPic;
+		Texture2D frame;
+		public string message;
+		bool showPic = false;
+		//Texture2D background ;
 
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
+			frame = Resources.Load<Texture2D> ("frame");
+		}
 	
-	}
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+		}
 
-	void OnTriggerEnter(Collider col)
-	{
-		Debug.Log (message);
-	}
+		void OnGUI ()
+		{
+				if (showPic) {
+						GUI.DrawTexture (new Rect (Screen.width /2-131.25f, Screen.height - Screen.height / 3, 262.5f, 165f), frame);
+						GUI.DrawTexture (new Rect (Screen.width /2-131.25f, Screen.height - Screen.height / 3, 262.5f, 165f), infoPic);
+				}
+		}
+
+		void OnTriggerEnter (Collider col)
+		{	
+				showPic = true;
+				//Debug.Log (message);
+		}
+
+		void OnTriggerExit (Collider col)
+		{
+				showPic = false;
+				//Debug.Log (message);
+		}
 }
