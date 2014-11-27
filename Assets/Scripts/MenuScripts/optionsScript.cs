383df9d963e0f3639e8ebe4b28239c38680dfe00
cristinaNiculescu <crissa_alessa@yahoo.com>
2014-11-27 14:43:58 +0100

    added " options "
    with instructions and control info

--- Assets/Scripts/MenuScripts/optionsScript.cs
+++ Assets/Scripts/MenuScripts/optionsScript.cs
@@ -1,17 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class optionsScript : MonoBehaviour {
+
+	Text textDisplay;
+
+
+	void Start()
+	{	
+		GameObject text = GameObject.Find ("TextBox");
+		textDisplay = text.GetComponent<Text> ();
+	}
 
 	// Use this for initialization
 	public void instructions()
 	{
-	
+			textDisplay.text = " You have to help Milo get home from school.  \n" +
+						" You have to make sure he is not caught by shadows and reach the end of his journey. \n" +
+						" If he does...you'll see how darkness attempts to prevail. \n \n" +
+						" Do your best ! ";
 	}
 
 	public void controls ()
 	{
-	
+		textDisplay.text = 
+						" Forwards\t: 'W' or the up arrow \n" +
+						" Backwards\t: 'S' or the down arrow \n" +
+						" Left\t: 'A' or the left arrow \n" +
+						" Right\t: 'D' or the right arrow \n"+
+						" Flip camera\t: 'F' \n \n \n"+
+						" Special Ability\t: 'Space' or 'T' depending on case" ;
+
 	}
 
 	public void goBack()
