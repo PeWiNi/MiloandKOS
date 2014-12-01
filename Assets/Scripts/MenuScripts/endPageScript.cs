7e5e82974a9995b3f259a98ff57ec307c5a37d63
cristinaNiculescu <crissa_alessa@yahoo.com>
2014-12-01 11:34:43 +0100

    endPage awaiting y'alls roles
    for credits  - includes restart and quit buttons

---
+++ Assets/Scripts/MenuScripts/endPageScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endPageScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Restart()
+	{
+		Application.LoadLevel ("IntroMenuScene");
+		}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+
+}
