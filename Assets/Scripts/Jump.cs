96f4627f20dc6209c0f9488ec8381941a4970608
henr9994 <henrikpedersen1@hotmail.com>
2014-12-04 21:45:01 +0100

    Small change to the Jump mechanic
    It is now a little more reactive

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -6,7 +6,7 @@ public class Jump : MonoBehaviour
     Animator anim;
     int jumpingTrigger;
     int groundedBool;
-    
+    bool jumped;
     void Awake()
     {
         anim = GetComponent<Animator>();
@@ -14,13 +14,26 @@ public class Jump : MonoBehaviour
         groundedBool = Animator.StringToHash("Grounded");
     }
 	
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool(groundedBool))
+        {
+            jumped = true;
+            anim.SetTrigger(jumpingTrigger);
+            anim.SetBool(groundedBool, false);
+//            rigidbody.velocity = new Vector3(0.0f, 3.5f, 0.0f);
+        }
+    }
+
     void FixedUpdate()
     {
         // Jumping.
-        if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool(groundedBool))
+//        if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool(groundedBool))
+        if (!anim.GetBool(groundedBool) && jumped)
         {
-            anim.SetTrigger(jumpingTrigger);
-            anim.SetBool(groundedBool, false);
+            jumped = false;
+//            anim.SetTrigger(jumpingTrigger);
+//            anim.SetBool(groundedBool, false);
             rigidbody.velocity = new Vector3(0.0f, 3.5f, 0.0f);
         }
     }
